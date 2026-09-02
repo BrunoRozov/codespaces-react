0dbbca7cb75848ae94059b5e61ec538176362667
Bruno <bruno.rozov@voco.ee>
2026-09-02 08:42:15 +0000

    Jäi poolei, peab kopdus ära tegema

---
+++ src/component/Expentseitems.jsx
@@ -0,0 +1,14 @@
+const Expentseitems = () => {
+    return (
+        <div>
+            <div>Data</div>
+            <div>
+                <h2>Title</h2>
+                <div>Price</div>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Expentseitems;
