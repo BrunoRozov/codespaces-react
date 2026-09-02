0f911e8a1671211eba3f6f50396c7bc24714f2cf
Bruno <bruno.rozov@voco.ee>
2026-09-02 15:55:04 +0000

    Komponendi muutuvate kohtade väärtustamine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css';
 import Expentseitems from './component/Expentseitems';
 function App  () {
-    return (
-        <div className="App">
-            <Expentseitems/>
 
-        </div>
-    );
+    const expense = [
+        {
+            data: new Date(2026, 9, 2),
+            title: 'New book',
+            price: 30.99
+        },
+        {
+            data: new Date(2026, 9, 2),
+            title: 'New jeanse',
+            price: 99.99
+        }
+    ]
+ 
+        return (
+            <div className="App">
+                <Expentseitems data={expense[0]}/>
+                <Expentseitems data={expense[1]}/>
+
+            </div>
+        );
 }
 
-export default App;
+    export default App;
--- src/component/Expentseitems.jsx
+++ src/component/Expentseitems.jsx
@@ -1,12 +1,15 @@
 import './Expentseitems.css'
 
-const Expentseitems = () => {
+const Expentseitems = (props) => {
+    
+    console.log(props)
+    console.log(props.data)
     return (
         <div className='expense-item'>
-            <div>Data</div>
+            <div>{props.data.data.toString()}</div>
             <div className='expense-item_description'>
-                <h2>Title</h2>
-                <div className='expense-item_price'>Price</div>
+                <h2>{props.data.title}</h2>
+                <div className='expense-item_price'>€{props.data.price.toFixed(2)}</div>
             </div>
         </div>
     )
