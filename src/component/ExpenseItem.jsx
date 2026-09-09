af18a7841d086968eab6328658c11e71c12ba5c0
Bruno <bruno.rozov@voco.ee>
2026-09-09 15:32:54 +0000

    Türa need fucking kriipsud ül1 done

--- src/component/ExpenseItem.jsx
+++ src/component/ExpenseItem.jsx
@@ -5,10 +5,10 @@ const ExpenseItem = (props) => {
 
   return (
     <div className='expense-item'>
-      <ExpenseDate date={props.date.date}/>
-      <div className='expense-item_description'>
-        <h2>{props.date.title}</h2>
-        <div className='expense-item_price'>{props.date.price}</div>
+      <ExpenseDate date={props.data.date}/>
+      <div className='expense-item__description'>
+        <h2>{props.data.title}</h2>
+        <div className='expense-item__price'>{props.data.price}</div>
       </div>
     </div>
   )
