2c5e7363ae953d218b07750eef77a760957571b4
Bruno <bruno.rozov@voco.ee>
2026-09-14 08:44:20 +0000

    ül 11 jäi pooleli

---
+++ src/component/NewExpense/ExpenseForm.jsx
@@ -0,0 +1,32 @@
+import './ExpenseForm.css'
+
+const ExpenseForm = (props) => {
+
+    const titleChangeHandler = (event) => {
+        console.log('Title changed!')
+    }
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text"
+                    onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Price</label>
+                    <input type="number" min="0" step="0.01" />
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2024-11-12" max="2026-01-31"/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm
